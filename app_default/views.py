913098c597da9141da611b7924d6dac938990d5e
yun <sys394880@gmail.com>
2021-12-24 00:04:32 +0900

    seven commit - p.79  간단한 폼 만들기(detail.html, result.html)

--- app_default/views.py
+++ app_default/views.py
@@ -1,7 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -18,9 +19,21 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'app_default/result.html', {'question' : question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'app_default/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
