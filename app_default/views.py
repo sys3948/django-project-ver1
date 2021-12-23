f0cba89fa18eed6a1f6d57efc36a38e6321418bd
yun <sys394880@gmail.com>
2021-12-24 00:13:17 +0900

    eigth commit - p.80 제네릭 뷰로 변경작업.

--- app_default/views.py
+++ app_default/views.py
@@ -1,39 +1,23 @@
+from django.db import models
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
+from django.views import generic
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list' : latest_question_list,
-    }
-    return render(request, 'app_default/index.html', context) # loder함수를 이용해서 template을 출력하는 방식말고 render 함수로 사용하여 편리하게 응답하기.
+class IndexView(generic.ListView):
+    template_name = 'app_default/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published question."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id) # 404 error를 발생시키기 위해서는 Http404객체를 사용해야한다. 그러나 그 방법보다 더 편한 객체(get_object_or_404)를 이용하는 방법.
-    return render(request, 'app_default/detail.html', {'question' : question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'app_default/detail.html'
 
 
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app_default/result.html', {'question' : question})
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'app_default/detail.html', {
-            'question' : question,
-            'error_message' : "You didn't select a choice.",
-        })
-    else:
-        selected_choice.vote += 1
-        selected_choice.save()
-
-        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+class ResultsView(generic.DeleteView):
+    models = Question
+    template_name = 'app_default/result.html'
