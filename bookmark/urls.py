805e3179a0afd2d6afe85483ff8ec95545ac9cec
yun <sys394880@gmail.com>
2021-12-28 01:09:03 +0900

    twelve commit - p.110 북마크 추가 기능 구현

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'), # 첫 번째 인자는 URL/ 다음의 내용, 두 번째 인자는 뷰함수 or 뷰 클래스로 클래스일 경우 as_view()를 사용해야한다. 세 번째 인자는 해당 name 값으로 URL 패턴을 찾을 수 있도록 하는 역활이다.
+    path('add/', BookmarkCreateView.as_view(), name='add'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 # Create your views here.
 
 class BookmarkListView(ListView):
     model = Bookmark
-    template_name = 'bookmark/bookmark_list.html'
+    # template_name = 'bookmark/bookmark_list.html'
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url'] # 어떤 필드들을 입력받을 것인지 설정하는 부분.
+    success_url = reverse_lazy('list') # 글 작성 완료후 이동할 페이지
+    template_name_suffix = '_create'
+    # template_name = 'bookmark/bookmark_create.html'
