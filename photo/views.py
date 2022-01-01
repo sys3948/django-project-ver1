41957bf62db88656a6bea28f9a3b58b71f5a9060
yun <sys394880@gmail.com>
2022-01-01 12:01:51 +0900

    tweenty-three commit - p.212  권한 제한하기 login_required 데코레이션 & LoginRequiredMixin object 설정 변경하기.

--- photo/views.py
+++ photo/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Photo
 
 # Create your views here.
-@login_required
+@login_required(redirect_field_name='', login_url='/account/login/')
 def photo_list(request):
     photos = Photo.objects.all()
+    print(request.GET.get("next"))
     print(photos)
     return render(request, 'photo/list.html', {'photos' : photos})
 
@@ -16,6 +17,9 @@ class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/upload.html'
+
+    login_url = '/account/login'
+    redirect_field_name =''
 
     def form_valid(self, form):
         form.instance.author_id = self.request.user.id
@@ -29,10 +33,18 @@ class PhotoUploadView(LoginRequiredMixin, CreateView):
 class PhotoDeleteView(LoginRequiredMixin, DeleteView):
     model = Photo
     success_url = '/'
+
+    login_url = '/account/login'
+    redirect_field_name =''
+
     template_name = 'photo/delete.html'
 
 
 class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
+
+    login_url = '/account/login'
+    redirect_field_name =''
+    
     template_name = 'photo/update.html'
